4458e6a312a65e4ec59db3820f6dc57cd2829166
kython-c <77231491+kython-c@users.noreply.github.com>
2021-11-18 11:47:27 +0300

    hotfix

--- src/commands/get_stat.js
+++ src/commands/get_stat.js
@@ -2,11 +2,11 @@ import { hearManager } from "../bot.js";
 import { Statistics } from "../repository/stat.js";
 
 export function stat_command() {
-    hearManager.hear(/^(?:stat)$/i, async (context) => {
+    hearManager.hear(/^(?:stat|стат)$/i, async (context) => {
         let stat = Statistics.getStat();
-        let message = "Статистика по командам:\n"
+        let message = "Статистика по командам:"
         for (let command of stat.items) {
-            message += `\n"${command.name}"\n\tКоличество вызовов - ${command.count}\n\tСреднее время вызова - ${Math.round(command.time)} ms`
+            message += `\n\n"${command.name}"\n\tКоличество вызовов - ${command.count}\n\tСреднее время вызова - ${Math.round(command.time)} ms`
         }
         await context.send(message);
 
